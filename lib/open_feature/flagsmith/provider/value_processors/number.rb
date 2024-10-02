7a066b586292907774fd0883896363171772de4c
Ben Chauvette <ben@chvtt.net>
2024-10-02 08:38:17 -0400

    fix boolean handling (#6)
    This commit updates the boolean handling in two ways:
    
    - it uses the flag's enabled status as the source of truth if the value is nil
    - it treats a blank string as true
    
    These changes are to accomodate quirks of setting flag values in the Flagsmith web UI.

--- lib/open_feature/flagsmith/provider/value_processors/number.rb
+++ lib/open_feature/flagsmith/provider/value_processors/number.rb
@@ -4,7 +4,7 @@ module OpenFeature
   module Flagsmith
     class Provider
       module ValueProcessors
-        Number = lambda do |value|
+        Number = lambda do |_flag, value|
           return value if value.is_a?(Numeric)
 
           raise TypeMismatchError, "Flag value is not a number"
