99f9523681ac9c3aa9bd90d94eb7a4940beb46f4
Ben Chauvette <bchauvette@forwardfinancing.com>
2024-10-02 01:06:09 -0400

    fix error codes
    - remove stale imports
    - update class references

--- lib/open_feature/flagsmith/provider/refinements/error_refinements.rb
+++ lib/open_feature/flagsmith/provider/refinements/error_refinements.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-require "open_feature/flagsmith/provider/error_code"
+require "open_feature/sdk/provider/error_code"
 require "flagsmith/sdk/errors"
 require "flagsmith/sdk/models/flags"
 
@@ -14,31 +14,31 @@ module OpenFeature
         module ErrorRefinements
           refine ::StandardError do
             def open_feature_error_code
-              ErrorCode::GENERAL
+              SDK::Provider::ErrorCode::GENERAL
             end
           end
 
           refine ::Flagsmith::Flags::NotFound do
             def open_feature_error_code
-              ErrorCode::FLAG_NOT_FOUND
+              SDK::Provider::ErrorCode::FLAG_NOT_FOUND
             end
           end
 
           refine ::Flagsmith::FeatureStateNotFound do
             def open_feature_error_code
-              ErrorCode::FLAG_NOT_FOUND
+              SDK::Provider::ErrorCode::FLAG_NOT_FOUND
             end
           end
 
           refine ::Flagsmith::ClientError do
             def open_feature_error_code
-              ErrorCode::FLAGSMITH_CLIENT_ERROR
+              SDK::Provider::ErrorCode::FLAGSMITH_CLIENT_ERROR
             end
           end
 
           refine ::Flagsmith::APIError do
             def open_feature_error_code
-              ErrorCode::FLAGSMITH_API_ERROR
+              SDK::Provider::ErrorCode::FLAGSMITH_API_ERROR
             end
           end
         end
